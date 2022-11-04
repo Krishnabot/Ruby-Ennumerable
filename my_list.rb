84e213646e471d833b08fbeed867ce538bd6b907
Krishna <libraryismyparadise@gmail.com>
2022-11-04 14:10:02 +0545

    Add test case to test class MyList

--- my_list.rb
+++ my_list.rb
@@ -1,3 +1,5 @@
+require_relative 'enumerable'
+
 class MyList
   include MyEnumerable
 
@@ -10,3 +12,22 @@ class MyList
 
     @list.each { |element| block.call(element) }
   end
+end
+
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts(list.all? { |e| e < 5 }) # true
+puts(list.all? { |e| e > 5 }) # false
+
+# Test #any?
+puts(list.any? { |e| e == 2 })
+# => true
+puts(list.any? { |e| e == 5 })
+# => false
+
+# Test #filter
+puts(list.filter(&:even?))
+# => [2, 4]
